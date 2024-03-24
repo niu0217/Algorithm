6f0d1f0d4842f2b4a761e482859a6c417d3a2399
niu0217 <1033856427@qq.com>
2024-03-24 17:52:35 +0800

    修改LeetCode34

--- Array/LeetCode34.cpp
+++ Array/LeetCode34.cpp
@@ -44,3 +44,21 @@ public:
         return {firstPosition, lastPosition};
     }
 };
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        auto iterFirst = lower_bound(nums.begin(), nums.end(), target);
+        int firstIndex = distance(nums.begin(), iterFirst);
+
+        //没有值大于等于target或者没有值等于target
+        if(iterFirst == nums.end() || *iterFirst != target) {
+            return {-1, -1};
+        }
+        else {
+            auto iterLast = upper_bound(nums.begin(), nums.end(), target) - 1;
+            int lastIndex = distance(nums.begin(), iterLast);
+            return {firstIndex, lastIndex};
+        }
+    }
+};
